9b0933284f59561380cc0df073bc63adba217100
PaimonLumine <86653653+PaimonLumine@users.noreply.github.com>
2022-07-07 09:00:51 +0800

    first commit

---
+++ 20220706.js
@@ -0,0 +1,30 @@
+console.log("Hello world!");
+
+let x;
+x = "This is x";
+console.log(x);
+x = 1111;
+console.log(x);
+
+x = 'JavaScript'; // Single quote marks also delimit strings.
+console.log(x);
+x = true; // A Boolean value.
+console.log(x);
+x = false; // The other Boolean value.
+console.log(x);
+x = null; // Null is a special value that means "no value."
+console.log(x);
+x = undefined; // Undefined is another special value like null.
+
+let book = {
+    topic: "JavaScript",
+    edition: 7 
+};
+
+console.log(book);
+console.log(book["edition"]);
+book.author = "Blablabla";
+book.content = {};
+console.log(book["author"]);
+console.log(book["content"]);
+
